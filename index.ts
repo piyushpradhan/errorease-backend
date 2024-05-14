cb897a6d847aff18146de8054e56f86a66c69fa5
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2024-05-15 01:46:06 +0530

    feat: setup basic github OAuth

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import passport from "passport";
+import session from "express-session";
 
 import router from "./routes/index";
+import "./strategies/passport-github";
 
 dotenv.config();
 
@@ -11,11 +14,14 @@ const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: 'nothing', resave: false, saveUninitialized: false }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 
 // Routes
 app.use("/api", router);
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || 3000, () => {
   //   console.log(`Server is running on port ${process.env.PORT}`);
 });
---
+++ strategies/passport-github.ts
@@ -0,0 +1,31 @@
+import passport from "passport";
+import dotenv from "dotenv";
+import { Strategy } from "passport-github2";
+
+dotenv.config();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+})
+
+passport.deserializeUser(function(obj, done) {
+  // TODO: Deserialize user using the database schema
+  // try {
+  //   const findUser = await DiscordUser.findById(id);
+  //   return findUser ? done(null, findUser) : done(null, null);
+  // } catch (err) {
+  //   done(err, null);
+  // }
+  done(null, obj as Express.User);
+});
+
+export default passport.use(new Strategy({
+  clientID: process.env.GITHUB_CLIENT_ID || "",
+  clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+  callbackURL: "http://localhost:3000/api/auth/callback/github"
+}, function(accessToken: string, refreshToken: string, profile: any, done: any) {
+  process.nextTick(function() {
+    return done(null, profile)
+  })
+}))
+
